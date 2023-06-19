67b3e4555f125b9108be80ac9f7f24342e9e9b03
ashish.purseth <ashishpurseth88@gmail.com>
2023-06-19 18:52:46 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,73 @@
+import { useRef } from "react";
+import "./App.css";
+
+function App() {
+  const name = useRef(null);
+  const age = useRef(null);
+
+  function onSubmit() {
+    const data: object = {
+      name: name.current?.value,
+      age: age.current?.value,
+    };
+    console.log(data);
+  }
+
+  const obj1 = {
+      ac:2,
+      anotherObj:{
+        anotherAnotherObj:{
+          aa:2,
+          a:5
+        }
+      }
+  }
+
+  const obj2 = {
+      ...obj1,
+      anotherObj:{
+        ...obj1.anotherObj,
+        anotherAnotherObj:{
+          ...obj1.anotherObj.anotherAnotherObj,
+          a:6
+        }
+      }
+  }
+
+  console.log(obj2);
+
+  return (
+    <>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6">
+            <div className="mb-2">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                name="name"
+                ref={name}
+                className="form-control"
+              />
+            </div>
+            <div className="mb-2">
+              <label htmlFor="age">Age</label>
+              <input
+                type="text"
+                name="age"
+                ref={age}
+                className="form-control"
+              />
+            </div>
+            <button className="btn btn-success" onClick={onSubmit}>
+              SUBMIT
+            </button>
+          </div>
+          <div className="col-md-6"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
